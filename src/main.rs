aac79b5abfd802f2ff5eba5ae486b22efca14b49
TheEggShark <theeggshark@gmail.com>
2022-05-03 12:06:05 -0500

    made it snake_case

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@
 use std::io;
 use std::sync::mpsc::channel;
 use std::thread;
+use std::fs;
 
 fn main() {
-    println!("{:?}", "Hello there general".chars().position(|c| c == ' ').unwrap());
-
     let (send, recv) = channel();
     thread::spawn(move || {
         loop {
@@ -26,11 +25,16 @@ fn main() {
                         file.pop();
                     }
                 }
-                // split into vector
-                let args = args_spliter(file.as_str()); 
+                let args = args_spliter(file.as_str());
                 match args[0] {
                     "play" => {
-                        play();
+                        // makes sure the user specified something to play
+                        if args.len() < 2 {
+                            println!("no Song speicifed");
+                        }
+                        else{
+                            play(args[1]);
+                        }
                     },
                     "pause" => {
                         pause();
@@ -45,17 +49,23 @@ fn main() {
     }
 }
 
-fn play(){
-    println!("play");
+fn play(fileName: &str){
+    // find file
+    // async play ---> may require a second channel :)
+    // pause when pause is entered
+    let filePath = String::from("music/") + fileName;
+    let contents = fs::read_to_string(filePath).expect("Something went wrong reading the file"); 
+    println!("play {}", contents);
 }
 
 fn pause(){
+    // make a value true --> false then back to true while sending it to a diffrent thread or somehow notifying the player
     println!("pause");
 }
 
 fn args_spliter(arg: &str) -> Vec<&str>{
     let mut vector = Vec::new();
-    let mut lastSpace = 0;
+    let mut last_space = 0;
     let mut i = 0;
     loop {
         if i >= arg.len() {
@@ -64,33 +74,32 @@ fn args_spliter(arg: &str) -> Vec<&str>{
         let letter = arg.chars().nth(i).unwrap();
         if letter == '\"'{
             let split = &arg[i+1..];
-            let endQoute = split.find("\"");
-            let endQoute = match endQoute{
+            let end_qoute = split.find("\"");
+            let end_qoute = match end_qoute{
                 Some(x) => x,
                 None => panic!("No End Qoute"), // temperarry error handling need more robust later
             };
-            vector.push(&split[..endQoute]);
-            lastSpace = i + endQoute + 3; // assumes there is a space after qoutes could cuase many bugs
-            i += endQoute + 3; // I fixed a bug but now it works dont touch this line
+            vector.push(&split[..end_qoute]);
+            last_space = i + end_qoute + 3; // assumes there is a space after qoutes could cuase many bugs
+            i += end_qoute + 3; // I fixed a bug but now it works dont touch this line
         }
         else if letter == '\''{
             let split = &arg[i+1..];
-            let endQoute = split.find("\'");
-            let endQoute = match endQoute{
+            let end_qoute = split.find("\'");
+            let end_qoute = match end_qoute{
                 Some(x) => x,
                 None => panic!("No End Qoute"), // temperarry error handling need more robust later
             };
-            vector.push(&split[..endQoute]);
-            lastSpace = i + endQoute + 3; // assumes there is a space after qoutes could cuase many bugs
-            i += endQoute + 3; // I fixed a bug but now it works dont touch this line
+            vector.push(&split[..end_qoute]);
+            last_space = i + end_qoute + 3; // assumes there is a space after qoutes could cuase many bugs
+            i += end_qoute + 3; // I fixed a bug but now it works dont touch this line
         }
         else if letter == ' '{
-            vector.push(&arg[lastSpace..i]);
-            lastSpace = i+1;
+            vector.push(&arg[last_space..i]);
+            last_space = i+1;
         }
         i += 1;
     }
-    vector.push(&arg[lastSpace..]);
-    println!("{:?}", vector);
+    vector.push(&arg[last_space..]);
     vector
 }
